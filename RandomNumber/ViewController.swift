e3ef193e27998903644318d43b07cb9ba8e6ebc7
Taiki <tyoshikawa1106@gmail.com>
2015-10-29 13:22:06 +0900

    commit

--- RandomNumber/ViewController.swift
+++ RandomNumber/ViewController.swift
@@ -10,16 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var even_or_odd: UILabel!
+    
+    @IBAction func startButton(sender: AnyObject) {
+        let retNum = Int(arc4random_uniform(50))
+        print("Number = \(retNum)")
+        
+        resultLabel.text = String(retNum)
+        
+        let surplus = retNum % 2
+        print("Surplus = \(surplus)")
+        if surplus == 0 {
+            even_or_odd.text = "『Even』"
+        } else {
+            even_or_odd.text = "『Odd』"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print("START!")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
-
